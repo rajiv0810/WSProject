6e9a88f5ee7646e35f0284a4512bae999d3cba58
Raju Singh <rajiv.icps@gmail.com>
2018-09-07 12:09:46 +0530

    course-mgt using soap WS

---
+++ course-management/src/main/java/com/in28minutes/soap/webservices/coursemanagement/CourseDetailsEndpoint.java
@@ -0,0 +1,31 @@
+package com.in28minutes.soap.webservices.coursemanagement;
+
+import org.springframework.ws.server.endpoint.annotation.Endpoint;
+import org.springframework.ws.server.endpoint.annotation.PayloadRoot;
+import org.springframework.ws.server.endpoint.annotation.RequestPayload;
+import org.springframework.ws.server.endpoint.annotation.ResponsePayload;
+
+import com.in28minutes.courses.CourseDetails;
+import com.in28minutes.courses.GetCourseDetailsRequest;
+import com.in28minutes.courses.GetCourseDetailsResponse;
+
+@Endpoint
+public class CourseDetailsEndpoint {
+
+	
+	@PayloadRoot(namespace = "http://in28minutes.com/courses", localPart = "GetCourseDetailsRequest")
+	@ResponsePayload
+	public GetCourseDetailsResponse processCourseDetailsRequest(@RequestPayload GetCourseDetailsRequest request) {
+		GetCourseDetailsResponse response = new GetCourseDetailsResponse();
+		
+		CourseDetails courseDetails = new CourseDetails();
+		courseDetails.setId(request.getId());
+		courseDetails.setName("Microservices Course");
+		courseDetails.setDescription("That would be a wonderful course!");
+		
+		response.setCourseDetails(courseDetails);
+		
+		return response;
+	}
+
+}
